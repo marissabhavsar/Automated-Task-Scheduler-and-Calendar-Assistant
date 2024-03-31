168846bc773cf04fc91a9b9ebee4785332c5abb7
Varun-Dutta <72882713+Varun-Dutta@users.noreply.github.com>
2024-03-31 11:09:06 -0400

    LangChain Update
    New Langchain file that works like the GPT file

--- fully working/langchain_methods.py
+++ fully working/langchain_methods.py
@@ -1,24 +1,24 @@
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains.conversation.memory import ConversationBufferMemory, ConversationEntityMemory
 
 class LLM:
-    def __init__(self, name, llm):
+    def __init__(self, name, llm, memory, calendar):
         self.name = name
         self.llm = llm 
+        self.memory = memory
+        self.calendar = calendar
 
-def get_user_choice():
+def get_user_choice(calendar):
     print("Please input your choice of LLM")
     choice = input()
 
     while not choice.isdigit():
         print("Please enter a number")
         choice = input()
-
-    if choice == 0:
-        return LLM("ChatGPT", ChatOpenAI(openai_api_key="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"))
-
-    else:
-        return LLM("ChatGPT", ChatOpenAI(openai_api_key="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"))
+    
+    llm = ChatOpenAI(openai_api_key="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")
+    return LLM("ChatGPT", llm, ConversationBufferMemory(), calendar)
     
 def get_user_query():
     print("What change would you like to make to your calendar")
